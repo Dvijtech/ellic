9b14304dee8f56eafd289d554fdcacf0078dad58
Dim <dselog@gmail.com>
2026-03-03 17:48:19 +0300

    работают левый и правый мотор норм. при включени ничего не стартует без разрешения. н ооба колеса вращаются в одну сторону. видимо надо поменять в одном подключение. но пока непонятно как

--- Scooter/src/motor.cpp
+++ Scooter/src/motor.cpp
@@ -10,11 +10,22 @@ void motorInit() {
     //digitalWrite(PIN_DIR_LEFT, HIGH);
     //digitalWrite(PIN_DIR_RIGHT, HIGH);
 
+    // КРИТИЧНО: сначала сделать PWM-пины обычным OUTPUT и обнулить
+    pinMode(PIN_PWM_LEFT, OUTPUT);
+    pinMode(PIN_PWM_RIGHT, OUTPUT);
+
+    digitalWrite(PIN_PWM_LEFT, LOW);
+    digitalWrite(PIN_PWM_RIGHT, LOW);
+
+    delay(100);  // дать драйверу стабилизироваться
+
     ledcSetup(0, PWM_FREQ, PWM_RESOLUTION);
     ledcAttachPin(PIN_PWM_LEFT, 0);
+    ledcWrite(0, 0);
 
     ledcSetup(1, PWM_FREQ, PWM_RESOLUTION);
     ledcAttachPin(PIN_PWM_RIGHT, 1);
+    ledcWrite(1, 0);
 }
 
 static int percentToPWM(int percent) {
