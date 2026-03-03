a1390c04b9d6ecebce641de7307afce11989d238
Dim <dselog@gmail.com>
2026-03-03 17:29:22 +0300

    заработали оба мотора влево вправо, но при старте происходит рывок ипервое время вращене обоих колес

--- Scooter/src/joystick.cpp
+++ Scooter/src/joystick.cpp
@@ -2,16 +2,36 @@
 #include "config.h"
 #include "joystick.h"
 
+static int center = 2048;
+
 void joystickInit() {
     pinMode(PIN_JOYSTICK_X, INPUT);
+
+    delay(500); // стабилизация
+
+    // авто-калибровка центра
+    long sum = 0;
+    for (int i = 0; i < 100; i++) {
+        sum += analogRead(PIN_JOYSTICK_X);
+        delay(5);
+    }
+    center = sum / 100;
+
+    Serial.print("Joystick center: ");
+    Serial.println(center);
 }
 
 Zone readJoystick() {
-    int raw = analogRead(PIN_JOYSTICK_X);
-    float percent = (raw / 4095.0) * 100.0;
 
-    if (percent < Deadzone) return ZONE_LEFT;
-    if (percent > (100 - Deadzone)) return ZONE_RIGHT;
+    int value = analogRead(PIN_JOYSTICK_X);
+
+    int threshold = (Deadzone / 100.0) * 4095.0;
+
+    if (value < center - threshold)
+        return ZONE_LEFT;
+
+    if (value > center + threshold)
+        return ZONE_RIGHT;
 
     return ZONE_DEAD;
 }
--- Scooter/src/main.cpp
+++ Scooter/src/main.cpp
@@ -14,12 +14,15 @@ unsigned long lastUpdate = 0;
 
 void setup() {
     Serial.begin(115200);
-
+    analogSetPinAttenuation(PIN_JOYSTICK_X, ADC_11db); // Настройка АЦП для джойстика (полный диапазон 0–3.3В)
     joystickInit();
     motorInit();
 }
 
 void loop() {
+
+    Serial.println(analogRead(PIN_JOYSTICK_X));
+    delay(200);
 
     if (millis() - lastUpdate >= LOOP_PERIOD_MS) {
         lastUpdate = millis();
