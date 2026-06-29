da0cb07bc4de1be8a8a1ef6ab295e5da57da3edd
Dim <dselog@gmail.com>
2026-06-29 13:48:45 +0300

    подключил энкодер

--- firmware/ESP32ELLIC/Experiments/AS5600/src/main.cpp
+++ firmware/ESP32ELLIC/Experiments/AS5600/src/main.cpp
@@ -1,14 +1,29 @@
 #include <Arduino.h>
+#include <Wire.h>
+#include <AS5600.h>
 
-void setup() {
+AS5600 encoder;
+
+void setup()
+{
     Serial.begin(115200);
-    delay(3000);
 
-    Serial.println();
-    Serial.println("===== START =====");
+    Wire.begin(21,22);
+
+    if (!encoder.begin())
+    {
+        Serial.println("Encoder NOT found");
+        while(true);
+    }
+
+    Serial.println("Encoder OK");
 }
 
-void loop() {
-    Serial.println("ESP32 OK");
-    delay(1000);
+void loop()
+{
+    float angle = encoder.readAngle() * 360.0 / 4096.0;
+
+    Serial.println(angle);
+
+    delay(20);
 }
